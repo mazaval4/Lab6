ed48d0770eff693f03eaf5b5498fc01a19cf8383
Miguel Zavala (Student) <mazaval4@sundevils.asu.edu>
2016-11-30 12:45:53 -0700

    im so tired

--- app/src/main/java/com/example/mazaval4/helloworld/HelloActivity.java
+++ app/src/main/java/com/example/mazaval4/helloworld/HelloActivity.java
@@ -3,6 +3,7 @@ package com.example.mazaval4.helloworld;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
@@ -18,9 +19,21 @@ public class HelloActivity extends AppCompatActivity {
         setContentView(R.layout.activity_hello);
         nextPage();
 
+        WebView webview = (WebView) findViewById(R.id.htmlRender);
+        String summary = "<html><body><br><br><center>In the last game " +
+                "name won after xxx rounds by guessing suspect in the room " +
+                "with a weapon</center></body></html>";
+        webview.loadData(summary, "text/html", null);
+
     }
 
+    @Override
+    public void onBackPressed() {
 
+        Intent i = new Intent (HelloActivity.this, HelloActivity.class);
+        startActivity(i);
+        super.onBackPressed();
+    }
 
     public void nextPage(){
         final Button button = (Button)findViewById(R.id.button);
@@ -47,19 +60,12 @@ public class HelloActivity extends AppCompatActivity {
                 startActivity(i);
                 }
 
+
             }
 
 
         });
 
 
-    }
-
-    @Override
-    public void onBackPressed() {
-
-        Intent i = new Intent (HelloActivity.this, HelloActivity.class);
-        startActivity(i);
-        super.onBackPressed();
     }
 }

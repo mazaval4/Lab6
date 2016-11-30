ba10d1ad7dfcb4f74156e7a26809a643306809ac
Miguel Zavala (Student) <mazaval4@sundevils.asu.edu>
2016-11-30 12:05:56 -0700

    activity 2 changes

--- app/src/main/java/com/example/mazaval4/helloworld/HelloActivity.java
+++ app/src/main/java/com/example/mazaval4/helloworld/HelloActivity.java
@@ -1,11 +1,13 @@
 package com.example.mazaval4.helloworld;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
-
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class HelloActivity extends AppCompatActivity {
@@ -14,25 +16,50 @@ public class HelloActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
-//        WebView myWebView = (WebView) findViewById(R.id.webView);
-//
-//        myWebView.setWebViewClient(new WebViewClient());
-//        myWebView.loadUrl("file:///android_asset/Clue.html");
-//        WebSettings ws = myWebView.getSettings();
-//        ws.setJavaScriptEnabled(true);
         nextPage();
 
     }
+
+
+
     public void nextPage(){
-        final Button map_button = (Button)findViewById(R.id.button);
-        map_button.setOnClickListener(new View.OnClickListener() {
+        final Button button = (Button)findViewById(R.id.button);
+        final EditText mEdit = (EditText) findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String name = mEdit.getText().toString();
+
+                if(name.isEmpty())
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please enter a name!";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+
+                else{
                 Intent i = new Intent (HelloActivity.this, HelloActivity2.class);
-                i.putExtra("name","miguel");
+                i.putExtra("name",name);
                 startActivity(i);
+                }
 
             }
+
+
         });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Intent i = new Intent (HelloActivity.this, HelloActivity.class);
+        startActivity(i);
+        super.onBackPressed();
     }
 }
--- app/src/main/java/com/example/mazaval4/helloworld/HelloActivity2.java
+++ app/src/main/java/com/example/mazaval4/helloworld/HelloActivity2.java
@@ -3,24 +3,62 @@ package com.example.mazaval4.helloworld;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class HelloActivity2 extends AppCompatActivity {
 
+
+    private static final String URL = "file:///android_asset/Clue.html";
+    private WebView myWebView;
+    static String name ="";
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello2);
+        myWebView = (WebView) findViewById(R.id.webView);
 
-        WebView myWebView = (WebView) findViewById(R.id.webView);
         Intent i = getIntent();
-        String name = i.getExtras().getString("name");
-        myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("file:///android_asset/Clue.html");
-        WebSettings ws = myWebView.getSettings();
-        ws.setJavaScriptEnabled(true);
+        name = i.getExtras().getString("name");
+        name = Character.toUpperCase(name.charAt(0)) + name.substring(1);
+
+
+        if(name.isEmpty())
+        {
+           finish();
+        }
+
+        myWebView.getSettings().setJavaScriptEnabled(true);
+
+        myWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url)
+            {
+                myWebView.loadUrl("javascript:getUserName(\"" + name + "\")");
+            }
+        });
+        myWebView.loadUrl(URL);
+
+        myWebView.addJavascriptInterface(new Object()
+        {
+            @android.webkit.JavascriptInterface
+            public void performClick(String temp){
+                Intent tired = new Intent(HelloActivity2.this,HelloActivity.class);
+                tired.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(tired);
+            }
+        },"cont");
+
 
     }
+
+
 }
+
+
+
